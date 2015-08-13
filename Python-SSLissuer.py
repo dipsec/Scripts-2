4a677e82f1fc2d9c9536366e07ac5491e7158684
Jamie Shaw <jshaw87@gmail.com>
2015-08-13 14:04:49 +0100

    Modified SSLIssuer Script
    Modified SSLIssuer Script

--- Python-SSLissuer.py
+++ Python-SSLissuer.py
@@ -2,23 +2,17 @@
 # Description: Parses an Nmap XML file to pull out the SSL certificate issuer. This relies on the ssl-cert nse script being used.
 # Usage: ./Python-SSLissuer.py <nmapscan.xml>
 
-import xml.etree.ElementTree as ET
+from lxml import etree
 import sys
 
 nmapFile = sys.argv[1]
 
-tree = ET.parse(nmapFile)
-root = tree.getroot()
+xml = etree.parse(open(nmapFile))
 
-for host in root.iter('address'):
-    ip = host.attrib["addr"]
-
-for issuer in root.iter('script'):
-    certInfo = issuer.attrib["output"]
-    for line in certInfo.split("\n"):
-        if line.startswith("Issuer:"):
-            issuerInfo = line
-
-print
-print "Host: " + ip + " - " + issuerInfo
-print "-----------------------------------------------------"
+for host in xml.xpath("//host"):
+	for addr in host.xpath("address/@addr"):
+		print "------------------------------"
+		print "Host: " + addr
+	for port in host.xpath("ports/port"):
+		for issuer in port.xpath('script[@id="ssl-cert"]/table[@key="issuer"]/elem[@key="commonName"]'):
+			print "Issuer: " + issuer.text
